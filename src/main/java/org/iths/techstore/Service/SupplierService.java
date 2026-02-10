68e3abbacbf8332ab1bcf15c08f7da639c0b9d4d
AnwarAsry <anwareducation03@gmail.com>
2026-02-10 21:30:21 +0100

    Added Service annotation

--- src/main/java/org/iths/techstore/Service/SupplierService.java
+++ src/main/java/org/iths/techstore/Service/SupplierService.java
@@ -2,10 +2,12 @@ package org.iths.techstore.Service;
 
 import org.iths.techstore.Model.Supplier;
 import org.iths.techstore.Repository.SupplierRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.NoSuchElementException;
 
+@Service
 public class SupplierService {
     private final SupplierRepository supplierRepository;
 
