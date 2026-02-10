4a866396de7edbfb2b65bac4274ffe815fd38b75
AnwarAsry <anwareducation03@gmail.com>
2026-02-11 00:46:25 +0100

    Controller and html files for fetch all route

---
+++ src/main/java/org/iths/techstore/Controller/SupplierController.java
@@ -0,0 +1,23 @@
+package org.iths.techstore.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/supplier")
+public class SupplierController {
+
+    private final SupplierService supplierService;
+
+    public SupplierController(SupplierService supplierService) {
+        this.supplierService = supplierService;
+    }
+
+    @GetMapping
+    public String showSupplierPage(Model model) {
+        model.addAttribute("suppliers", supplierService.getAllSuppliers());
+        return "supplier";
+    }
+}
